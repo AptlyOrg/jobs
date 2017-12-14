e85e02da327bd896f5e41e4636182a229228ab31
Wes Hedrick <wes@inreturnstrategies.com>
2017-12-13 20:16:59 -0700

    Add sequencing that supports multiple downloads against Indeeds 25 job API limit

--- jobs.sh
+++ jobs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-declare -a arr=("Commerce%20Bank" "DST%20Systems" "C2FO" "Kansas%20City%20Public%20Library" "First%20National")   # "element3"
+declare -a arr=("Commerce-Bank" "DST-Systems" "C2FO" "Kansas-City-Public-Library" "First-National-Bank-of-Omaha")
 
 for i in "${arr[@]}"
 do
@@ -10,15 +10,14 @@ do
     printf '' > $decodedname.json
 
     curl -X GET \
-     "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&l=&v=2&q=company:\"$i\"&limit=25" \
+     "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&l=&v=2&start=[0-24:24]&limit=25&q=company:\"$i\"" \
       -H 'Accept: application/json' \
       -H 'Cache-Control: no-cache' \
       -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
       -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
-        | python -m json.tool >> $decodedname.json
+         >> $decodedname.json
 
+#    cat $decodedname.json | python -m json.tool > $decodedname.json
 done
 
 echo "...done"
-
-# echo "${arr[0]}", "${arr[1]}"
