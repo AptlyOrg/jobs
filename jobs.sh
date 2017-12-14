bb54b8622a6baba0eb8911bc3d2c0f5719e67b40
Wes Hedrick <wes@inreturnstrategies.com>
2017-12-14 09:45:58 -0700

    Add C&C Group to HOs list   Manually mend multi-page downloads   Clean up pretty filenames to exclude ugly search string

--- jobs.sh
+++ jobs.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-declare -a arr=("Commerce-Bank" "DST-Systems" "C2FO" "Kansas-City-Public-Library" "First-National-Bank-of-Omaha")
+declare -a arr=("Commerce-Bank"
+                "DST-Systems"
+                "C2FO"
+                "Kansas-City-Public-Library"
+                "First-National-Bank-of-Omaha"
+                "C-%26-C-Group-2")
 
 for i in "${arr[@]}"
 do
 
-    decodedname=`echo $i`
+    decodedname=`echo ${i//%26/&} | tr "[:upper:]" "[:lower:]"`
     limitcount=25
     startstring=""
 
@@ -15,7 +20,7 @@ do
           -H 'Cache-Control: no-cache' \
           -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
           -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
-        | grep totalResults | jq -r  '.totalResults')
+        | grep totalResults | jq -r '.totalResults')
 
     printf "\n\nPrepping $decodedname...\t $jobcount job(s) found!\n"
     printf '' > $decodedname.json
