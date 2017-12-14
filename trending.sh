f5d3a6601dd57e7be625463c87fda76bfdffea81
Wes Hedrick <wes@inreturnstrategies.com>
2017-12-14 15:47:02 -0700

    Add C&C Group to HOs list   Manually mend multi-page downloads   Clean up pretty filenames to exclude ugly search string

---
+++ trending.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+declare -a arr=("Commerce-Bank"
+                "DST-Systems"
+                "C2FO"
+                "Kansas-City-Public-Library"
+                "First-National-Bank-of-Omaha"
+                "C-%26-C-Group-2"
+                "Garmin")
+
+trendingname="trending"
+printf '' > $trendingname.json
+
+for i in "${arr[@]}"
+do
+
+    decodedname=`echo ${i//%26/&} | tr "[:upper:]" "[:lower:]"`
+    limitcount=25
+    startstring=""
+
+    jobcount=$(curl --silent --request GET  \
+        "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&l=&v=2&limit=1&fromage=1&q=company:\"$i\"" \
+          -H 'Accept: application/json' \
+          -H 'Cache-Control: no-cache' \
+          -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
+          -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
+        | grep totalResults | jq -r '.totalResults')
+
+if [ "$jobcount" -eq "0" ]
+then
+    printf "\n\nPrepping $decodedname... hmmmm, no jobs found... <sigh>\n"
+    printf "Suspended download.\n"
+else
+    printf "\n\nPrepping $decodedname...\t $jobcount job(s) found!\n"
+    printf "Retrieving job(s)...\n"
+fi
+
+if [ "$jobcount" -lt "$limitcount" ]
+then
+    limitcount=$jobcount
+else
+    startstring="&start=[0-$jobcount:$limitcount]"
+fi
+
+    curl -# --request GET \
+     "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&l=&v=2$startstring&limit=$limitcount&q=company:\"$i\"" \
+          -H 'Accept: application/json' \
+          -H 'Cache-Control: no-cache' \
+          -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
+          -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
+         >> $trendingname.json
+
+    printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+    printf "$decodedname download complete\n"
+    printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+
+#    cat $decodedname.json | python -m json.tool > $decodedname.json
+done
+
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "Job retrieval complete."
