ccc4d503352e42337cfc0557e17b66fec15c07de
Wes Hedrick <wes@inreturnstrategies.com>
2017-12-17 20:27:41 -0700

    Add C&C Group to HOs list   Manually mend multi-page downloads   Clean up pretty filenames to exclude ugly search string

--- jobs.sh
+++ jobs.sh
@@ -5,12 +5,22 @@ declare -a arr=("Commerce-Bank"
                 "C2FO"
                 "Kansas-City-Public-Library"
                 "First-National-Bank-of-Omaha"
-                "C-%26-C-Group-2"
+                "C%26C-Sales-Inc."
                 "Garmin")
+
+datestamp=`date`
+totaljobcount=0
+
+fileroot="./archive"
+totalfile="$fileroot/summary.txt"
+alljobs="$fileroot/alljobs.json"
+
+
+printf '' > $totalfile
+printf '' > $alljobs
 
 for i in "${arr[@]}"
 do
-
     decodedname=`echo ${i//%26/&} | tr "[:upper:]" "[:lower:]"`
     limitcount=25
     startstring=""
@@ -23,13 +33,16 @@ do
           -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
         | grep totalResults | jq -r '.totalResults')
 
+totaljobcount=$(($totaljobcount + $jobcount));
+printf "$decodedname -- $jobcount\n" >> $totalfile
+
 if [ "$jobcount" -eq "0" ]
 then
     printf "\n\nPrepping $decodedname... hmmmm, no jobs found... <sigh>\n"
     printf "Suspended download.\n"
 else
     printf "\n\nPrepping $decodedname...\t $jobcount job(s) found!\n"
-    printf '' > $decodedname.json
+    printf '' > "$fileroot/$decodedname.json"
     printf "Retrieving job(s)...\n"
 fi
 
@@ -46,7 +59,7 @@ fi
           -H 'Cache-Control: no-cache' \
           -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
           -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
-         >> $decodedname.json
+         | tee -a "$fileroot/$decodedname.json" "$alljobs.$datestamp"
 
     printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
     printf "$decodedname download complete\n"
@@ -56,7 +69,16 @@ fi
 done
 
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "  Job retrieval complete"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
-printf "Job retrieval complete."
+printf "  SUMMARY REPORT saved to file: $totalfile.$datestamp\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+
+cat $totalfile
+mv $totalfile "$totalfile.$datestamp"
+printf "Total job(s) consumed on $datestamp: <drum roll>...\n"
+printf "$totaljobcount"
