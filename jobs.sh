4e7215fbc070cc5348f276f3e4147bac90edb2ff
Wes Hedrick <wes@inreturnstrategies.com>
2018-01-04 11:15:58 -0700

    Provide command line support for the following args: location, jobtype, and verbose output.

--- jobs.sh
+++ jobs.sh
@@ -1,32 +1,108 @@
 #!/usr/bin/env bash
 
-declare -a arr=(`cat ho.txt`)
+declare -a orgs=(`cat ho.txt`)
+declare -a jobtypes=( fulltime parttime contract internship temporary )  # Defined by the Indeed API
+
+# Initialize command line args
+jobtype=''
+verbosity=0
+location=''
+match=''
+returnValue=0;
+
+containsElement() {
+    match="$1"
+    returnValue=1
+    for e in ${jobtypes[@]}; do
+        if [ "$e" == "$match" ]; then
+            returnValue=0
+        fi
+    done
+    return $returnValue
+}
+
+die() {
+    echo "$1" >&2
+    for e in ${jobtypes[@]}; do
+        echo "$e" >&2
+    done
+    exit 1
+}
+
+for var in $@; do
+    case $var in
+        -h|-\?|--help)
+            show_help                               # Display a usage synopsis.
+            exit
+            ;;
+        --jobtype=?*)
+            jobtype=${var#*=};
+            containsElement "$jobtype"
+            if [ "$returnValue" -eq 1 ]; then
+                message='ERROR: --jobtype must be one of the following:'
+                die "$message"
+            fi
+        ;;
+        --location=?*)
+            location=${var#*=};                     # Delete everything up to "=" and assign remainder
+            if [ "${#location}" -ne 5 ]; then
+                die 'ERROR: "--location" does not look quite right'
+            fi
+        ;;
+        --verbosity)
+            verbosity=1;
+        ;;
+        --)                                         # End of all options.
+            shift
+            break
+            ;;
+        -?*)
+            printf 'WARN: Unknown option (ignored): %s\n' "$1" >&2
+            ;;
+        *)                                          # Default case: No more options, break out of loop
+            break
+    esac
+    shift
+done
+
+if [ "$verbosity" -eq 1 ];  then echo 'Verbosity is ON';fi
+if [ "$jobtype" ]; then
+    echo 'Job Type was found, using:' "$jobtype";
+    jobTypeFile="$jobtype""."
+fi
+if [ "$location" ]; then
+    echo 'Location was found, using:' "$location";
+    locationFile="$location""."
+fi
 
 # Intialize
 datestamp=`date`
 totaljobcount=0
 fileroot="./archive"
-summaryfile="$fileroot/summary.txt"
-alljobs="$fileroot/alljobs.json"
+summaryfile=$fileroot"/"$locationFile$jobTypeFile"summary.txt"
+alljobs=$fileroot"/"$locationFile$jobTypeFile"alljobs.json"
+
+
 
 # Ephemeral file creation
 printf '' > $summaryfile
 
-for i in "${arr[@]}"
+for i in "${orgs[@]}"
 do
     decodedname=`echo ${i//%26/&} | tr -d '.' | tr '&' '-' | tr "[:upper:]" "[:lower:]"`
+    decodednameFile=$fileroot"/"$locationFile$jobTypeFile$decodedname".json"
     limitcount=25
     startstring=""
 
-
     # Pull a single record to determine size of the result set
     jobcount=$(curl --silent --request GET  \
-        "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&l=&v=2&limit=1&q=company:\"$i\"" \
+        "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&jt=$jobtype&l=$location&v=2&limit=1&q=company:\"$i\"" \
           -H 'Accept: application/json' \
           -H 'Cache-Control: no-cache' \
-          -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
           -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
         | grep totalResults | jq -r '.totalResults')
+
+    #  Probably unnecessary -- -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
 
     totaljobcount=$(($totaljobcount + $jobcount));
     printf "$decodedname -- $jobcount\n" >> "$summaryfile"
@@ -39,7 +115,7 @@ do
         printf "Suspended download.\n"
     else
         printf "\n\nPrepping $decodedname...\t $jobcount job(s) found!\n"
-        printf '' > "$fileroot/$decodedname.json"
+        printf '' > "$decodednameFile"
         printf "Retrieving job(s)...\n"
     fi
 
@@ -56,12 +132,13 @@ do
 
     # Retrieve records -- curl will use "start" and "limit" to iterate as appropriate
     curl -# --request GET \
-     "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&l=&v=2$startstring&limit=$limitcount&q=company:\"$i\"" \
+     "https://indeed-indeed.p.mashape.com/apisearch?publisher=127676247689188&format=json&jt=$jobtype&l=$location&v=2$startstring&limit=$limitcount&q=company:\"$i\"" \
           -H 'Accept: application/json' \
           -H 'Cache-Control: no-cache' \
-          -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
           -H 'X-Mashape-Key: YjtQBqSWKmmshwn5c06JmE2Ut5VSp1X5Z25jsn9laldG042Uoy' \
-         | tee -a "$fileroot/$decodedname.json" "$alljobs.$datestamp"
+         | tee -a "$decodednameFile" "$alljobs"
+
+    #  Probably unnecessary -- -H 'Postman-Token: bc9953ce-f706-08a3-bbf2-be05956fa3d2' \
 
     # Use something like the following to prettify json for readability if necessary
     #    cat $decodedname.json | python -m json.tool > $decodedname.json
@@ -78,10 +155,9 @@ done
 printf "  Job retrieval complete\n\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
-printf "  SUMMARY REPORT saved to file: $summaryfile.$datestamp\n"
+printf "  SUMMARY REPORT saved to file: $summaryfile\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 printf "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n"
 
 cat "$summaryfile"
-mv "$summaryfile" "$summaryfile.$datestamp"
 printf "Total jobs consumed on $datestamp: <drum roll>... $totaljobcount\n\n"
